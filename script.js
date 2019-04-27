6c526104c48208f88e064df8300148016b02a1c4
Irsan Ramli <irsan.ramli@gmail.com>
2019-04-27 11:47:30 +0700

    Menambahkan Variable dalam file script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Hello, world!!");
+
+//Membuat Heading di File Script
+document.getElementById('heading').innerHTML = "Hello, JS!!"
+
+//Membuat variable Nama
+var name = "Irsan";
+console.log(name);
+
+//Membuat variable LastName
+var lastName = "Ramli";
+console.log(lastName);
+
+//Membuat variable Umur
+var age = 26;
+console.log(age);
+
+//Membuat variable fulname+lastname
+var fullName = name + lastName; // concatenation.
+console.log(fullName);
+
+//Membuat variable Job (pekerjaan)
+var job = "Peserta";
+console.log(job);
+
+//Membuat variable biografi
+var bio = "My name " + name + " " + lastName + " " + " my age " + age + " my job " + job;
+console.log(bio);
